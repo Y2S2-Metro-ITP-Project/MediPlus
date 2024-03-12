c865f87f0078ccd97f48ff1e9acdb0c00252c824
Asiri Jayawardena <it22323934@my.sliit.lk>
2024-03-12 17:33:37 +0530

    feat:Database Connected

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => {console.log("Connected to database")}).catch(()=>{console.log("Connection failed")});
+
 
 const app = express();
 
