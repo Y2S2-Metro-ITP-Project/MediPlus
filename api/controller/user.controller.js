b7fda402d2a1be16d67fc0090ec64e8e568baa32
Asiri Jayawardena <it22323934@my.sliit.lk>
2024-03-13 22:16:48 +0530

    feat:Complete sidebar of the dashboard

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,3 +1,12 @@
 export const test=(req,res)=>{
     res.json({message:"API is working!!!!!"})
 }
+
+export const signout=(req,res,next)=>{
+    try {
+        res.clearCookie("token");
+        res.json({message:"Signout success"})
+    } catch (error) {
+        next(error)
+    }
+}
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { test } from "../controller/user.controller.js";
+import { test,signout} from "../controller/user.controller.js";
 const router=express.Router();
 
 router.get("/test",test)
+router.post("/signout",signout)
 
 export default router;
