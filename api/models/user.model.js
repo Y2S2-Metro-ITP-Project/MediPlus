11f14bf36d1dec780549637e1a1862a92d219cc2
Asiri Jayawardena <it22323934@my.sliit.lk>
2024-03-13 08:16:51 +0530

    feat:create user model

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,29 +1,32 @@
 import mongoose from "mongoose";
 
-const userSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
+const userSchema = new mongoose.Schema(
+  {
+    usernname: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+      type: String,
+      required: true,
     },
-    phone:{
-        type:String,
-        required:true
+    phone: {
+      type: String,
+      required: true,
     },
-    address:{
-        type:String,
-        required:true
+    address: {
+      type: String,
+      required: true,
     },
-},{timestamps:true});
+  },
+  { timestamps: true }
+);
 
-
-const User=mongoose.model('User',userSchema);
+const User = mongoose.model("User", userSchema);
 export default User;
