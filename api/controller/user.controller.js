ee31bff904aaaa0d2dfdabb4e51898101730ac0c
Asiri Jayawardena <it22323934@my.sliit.lk>
2024-03-14 01:26:28 +0530

    feat:Delete user API route

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -67,3 +67,19 @@ export const updateUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
+    return next(
+      errorHandler(403, "You are not allowed to delete this account")
+    );
+  }
+
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json({ message: "Account deleted successfully" });
+  } catch (error) {
+    next(error);
+  }
+  
+}
--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -3,7 +3,7 @@ import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useState } from "react";
 import { useRef } from "react";
-import { get } from "mongoose";
+import { get, set } from "mongoose";
 import { app } from "../firebase";
 import {
   getDownloadURL,
@@ -13,6 +13,19 @@ import {
 } from "firebase/storage";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
+import {
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure,
+  signOutSuccess,
+} from "../redux/user/userSlice";
+import {
+  updateStart,
+  updateSuccess,
+  updateFailure,
+} from "../redux/user/userSlice";
+import { useDispatch } from "react-redux";
+import { ToastContainer, toast } from "react-toastify";
 export default function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
   const [imageFile, setImageFile] = useState(null);
@@ -20,6 +33,10 @@ export default function DashProfile() {
   const filePickerRef = useRef();
   const [imageUploadProgress, setImageUploadProgress] = useState(null);
   const [imageUploadError, setImageUploadError] = useState(null);
+  const [formData, setFormData] = useState({});
+  const [imageFileUploading, setImageFileUploading] = useState(false);
+  const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
+  const dispatch = useDispatch();
   const handleEventChange = (e) => {
     const file = e.target.files[0];
     if (file) {
@@ -33,6 +50,7 @@ export default function DashProfile() {
     }
   }, [imageFile]);
   const uploadImage = async () => {
+    setImageFileUploading(true);
     setImageUploadError(null);
     const storage = getStorage(app);
     const fileName = new Date().getTime() + imageFile.name;
@@ -53,18 +71,58 @@ export default function DashProfile() {
         setImageUploadProgress(null);
         setImageFile(null);
         setImageFileUrl(null);
+        setImageFileUploading(false);
       },
       () => {
         getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
           setImageFileUrl(downloadURL);
+          setFormData({ ...formData, profilePicture: downloadURL });
+          setImageFileUploading(false);
         });
       }
     );
   };
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setUpdateUserSuccess(null);
+    if (Object.keys(formData).length === 0) {
+      toast.error("No changes to be made");
+      return;
+    }
+    if (imageFileUploading) {
+      toast.error("Please wait for image to upload before submitting");
+      return;
+    }
+    try {
+      dispatch(updateStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        dispatch(updateFailure(data.message));
+      } else {
+        dispatch(updateSuccess(data));
+        toast.success("Profile updated successfully");
+        setUpdateUserSuccess("Profile updated successfully");
+      }
+    } catch (error) {
+      setUpdateUserSuccess(error.message);
+      dispatch(updateFailure(error.message));
+    }
+  };
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
+      <ToastContainer />
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <input
           type="file"
           accept="image/*"
@@ -108,9 +166,20 @@ export default function DashProfile() {
           type="text"
           id="username"
           defaultValue={currentUser.username}
+          onChange={handleChange}
         />
-        <TextInput type="email" id="email" defaultValue={currentUser.email} />
-        <TextInput type="password" id="password" placeholder="**************" />
+        <TextInput
+          type="email"
+          id="email"
+          defaultValue={currentUser.email}
+          onChange={handleChange}
+        />
+        <TextInput
+          type="password"
+          id="password"
+          placeholder="**************"
+          onChange={handleChange}
+        />
         <Button type="submit" gradientDuoTone="purpleToBlue" outline>
           Update
         </Button>
@@ -119,6 +188,11 @@ export default function DashProfile() {
         <span className="cursor-pointer">Delete Account</span>
         <span className="cursor-pointer">Sign Out</span>
       </div>
+      {updateUserSuccess && (
+        <Alert color="success" className="mt-4">
+          {updateUserSuccess}
+        </Alert>
+      )}
     </div>
   );
 }
