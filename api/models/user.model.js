ce099e6d7f0e479d1558631e2fe6d88082b7ffe9
Asiri Jayawardena <it22323934@my.sliit.lk>
2024-03-13 08:10:56 +0530

    feat:Create the Header Section

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -7,12 +7,21 @@ const userSchema=new mongoose.Schema({
     },
     email:{
         type:String,
-        required:true
+        required:true,
+        unique:true
     },
     password:{
         type:String,
         required:true
-    }
+    },
+    phone:{
+        type:String,
+        required:true
+    },
+    address:{
+        type:String,
+        required:true
+    },
 },{timestamps:true});
 
 
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,9 +6,11 @@ import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import Dashboard from './pages/Dashboard'
 import ContactUs from './pages/ContactUs'
+import Header from './components/Header'
 export default function App() {
   return (
     <BrowserRouter>
+      <Header/>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
