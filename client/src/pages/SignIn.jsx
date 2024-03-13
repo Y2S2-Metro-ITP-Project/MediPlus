291ea9039799e782edef81c1d0bdf55d6867a301
Asiri Jayawardena <it22323934@my.sliit.lk>
2024-03-13 20:35:10 +0530

    feat:Make the dashboard Private

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -52,9 +52,9 @@ export default function SignIn() {
         <div className="flex-1">
           <Link to="/" className="font-bold dark:text-white text-4xl">
             <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-              Asiri's
+              Medi
             </span>
-            Blog
+            Plus
           </Link>
           <p className="text-sm mt-5">
             This is a demo project. You can use your email to sign in or the
