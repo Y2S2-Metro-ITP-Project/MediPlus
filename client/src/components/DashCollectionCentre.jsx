b5be982be848ba78b09474c44c066e7efb6d5fb6
Azri Muhsin <azrimuhsin246@gmail.com>
2024-04-03 04:34:46 +0530

    feat: lab dash and sidebar update

--- client/src/components/DashCollectionCentre.jsx
+++ client/src/components/DashCollectionCentre.jsx
@@ -13,10 +13,10 @@ import { useSelector } from "react-redux";
 
 export default function DashCollectionCentre() {
   const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(""); 
+  const [errorMessage, setErrorMessage] = useState("");
   const [loading, setLoading] = useState(false);
-  const {currentUser} = useSelector(state => state.user);
- // const navigate = useNavigate();
+  const { currentUser } = useSelector((state) => state.user);
+  // const navigate = useNavigate();
 
   const handleData = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
@@ -53,9 +53,7 @@ export default function DashCollectionCentre() {
   return (
     <div className="min-h-screen mt-20">
       <div className=" flex flex-col items-center justify-center">
-        <div className=" font-bold dark:text-white  text-4xl p-10 ">
-          Sample Collection
-        </div>
+        <h1 className=" font-semibold text-3xl">Sample Collection</h1>
         <div className=" bg-gray-200 dark:bg-slate-800 ">
           <form
             className="flex flex-col items-center justify-center gap-8"
@@ -104,13 +102,15 @@ export default function DashCollectionCentre() {
               <div className=" mb-3 block">
                 <Label className="" value="Sample collection Employee:" />
 
-                {currentUser? (
-                <span className="block text-sm ">
+                {currentUser ? (
+                  <span className="block text-sm ">
                     Employee name: @{currentUser.username}
-                </span>) : ( <span className="block text-base text-red-700 ">
-                  Authorized employee not logged in
-                </span>)
-                }
+                  </span>
+                ) : (
+                  <span className="block text-base text-red-700 ">
+                    Authorized employee not logged in
+                  </span>
+                )}
                 {/* <TextInput
                   type="text"
                   placeholder="Employee ID"
@@ -119,7 +119,12 @@ export default function DashCollectionCentre() {
                 /> */}
               </div>
             </div>
-            <Button className=" bg-gray-600 " type="submit" disabled={loading}>
+            <Button
+              gradientDuoTone="purpleToBlue"
+              outline
+              type="submit"
+              disabled={loading}
+            >
               {loading ? (
                 <>
                   <Spinner size="sm" />
--- client/src/components/DashLabSideBar.jsx
+++ client/src/components/DashLabSideBar.jsx
@@ -1,40 +1,81 @@
 import { Sidebar } from "flowbite-react";
-import {HiUser, HiArrowSmRight} from "react-icons/hi";
+import { HiUser, HiArrowSmRight } from "react-icons/hi";
 import { FaHouseMedical } from "react-icons/fa6";
 import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-
+import { ImLab } from "react-icons/im";
 
 export default function DashLabSideBar() {
-    const location = useLocation();
-    const [tab , setTab] = useState('')
-    useEffect(() => {
-  
-      const urlParams = new URLSearchParams(location.search)
-      const tabFromUrl = urlParams.get("tab")
-      
-      if(tabFromUrl){
-        setTab(tabFromUrl);
-      }
-  
-    }, [location.search]
-    );
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
   return (
-   
     <Sidebar className=" w-full md:w-56">
-        <Sidebar.Items>
-            <Sidebar.ItemGroup>
-                <Link to="/lab-dashboard?tab=sampleCollection">
-                <Sidebar.Item active={tab === "sampleCollection"} icon={FaHouseMedical} label={"User"} labelColor="dark">
-                    Collection Center
-                </Sidebar.Item>
-                </Link>
-                <Sidebar.Item  icon={HiArrowSmRight} className="cursor-pointer">
-                    Sign Out
-                </Sidebar.Item>
-            </Sidebar.ItemGroup>
-        </Sidebar.Items>
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Link to="/lab-dashboard?tab=sampleCollection">
+            <Sidebar.Item
+              active={tab === "sampleCollection"}
+              icon={FaHouseMedical}
+              label={"User"}
+              labelColor="dark"
+            >
+              Collection Center
+            </Sidebar.Item>
+          </Link>
+
+          {/* add current user trace option */}
+
+          <Link to=" ">
+            <Sidebar.Item
+              active={tab === "sampleManager"}
+              icon={ImLab}
+              as="div"
+            >
+              Sample Manager
+            </Sidebar.Item>
+          </Link>
+
+          <Link to=" ">
+            <Sidebar.Item active={tab === "testManager"} icon={ImLab} as="div">
+              Tests Manager
+            </Sidebar.Item>
+          </Link>
+
+          <Link to=" ">
+            <Sidebar.Item active={tab === "testOrder"} icon={ImLab} as="div">
+              Lab Test Order
+            </Sidebar.Item>
+          </Link>
+
+          <Link to=" ">
+            <Sidebar.Item
+              active={tab === "orderedTestManager"}
+              icon={ImLab}
+              as="div"
+            >
+              Test Orders Manager
+            </Sidebar.Item>
+          </Link>
+
+          <Link to=" ">
+            <Sidebar.Item active={tab === "labReports"} icon={ImLab} as="div">
+              Lab Reports
+            </Sidebar.Item>
+          </Link>
+
+          <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer">
+            Sign Out
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
     </Sidebar>
-    
   );
 }
