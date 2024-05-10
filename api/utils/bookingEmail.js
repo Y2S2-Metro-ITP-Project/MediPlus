ce1bd498abe33ecad32fcad30475b92d55cebd54
Ahamad Falah <mnfalahahamad@gmail.com>
2024-05-11 03:11:12 +0530

    feat: Update Google API scopes for bookingEmail and googleCalender utils

--- api/utils/bookingEmail.js
+++ api/utils/bookingEmail.js
@@ -5,7 +5,7 @@ import { authenticate } from '@google-cloud/local-auth';
 import { google } from 'googleapis';
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/gmail.send'];
+const SCOPES = ['https://www.googleapis.com/auth/gmail.send','https://www.googleapis.com/auth/meetings.space.created','https://www.googleapis.com/auth/calendar'];
 
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
