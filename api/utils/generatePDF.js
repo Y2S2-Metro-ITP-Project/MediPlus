0e97611cad50822ea941acec84be57e173bbb33f
Ahamad Falah <mnfalahahamad@gmail.com>
2024-05-10 11:57:41 +0530

    feat: Add email sending functionality using Gmail API

--- api/utils/generatePDF.js
+++ api/utils/generatePDF.js
@@ -1,31 +1,17 @@
-import html2pdf from 'html2pdf';
-import { Readable } from 'stream';
+import puppeteer from 'puppeteer';
 
-const generatePDF = (data, template) => {
-  return new Promise((resolve, reject) => {
-    const options = {
-      margin: 10, // Set the margin (in mm)
-      filename: 'report.pdf', // Set the output filename
-      image: { type: 'jpeg', quality: 0.98 }, // Set the output image quality (0-1)
-      enableLinks: false, // Enable or disable links
-      html2canvas: { scale: 2 }, // Set the rendering scale factor
-      jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }, // Set the PDF options
-    };
+async function generatePDFFromHtml(htmlContent) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-    const content = template(data);
-    const worker = html2pdf().set(options).from(content);
+  // Set content and wait for page to load
+  await page.setContent(htmlContent);
 
-    const stream = new Readable();
-    stream._read = () => {}; // Required to prevent the stream from being paused
+  // Generate PDF
+  const pdfBuffer = await page.pdf({ format: 'A4' });
 
-    worker.on('ok', (pdf) => {
-      stream.push(pdf);
-      stream.push(null); // Signal the end of the stream
-      resolve(stream);
-    }).on('error', (err) => {
-      reject(err);
-    });
-  });
-};
+  await browser.close();
+  return pdfBuffer;
+}
 
-export default generatePDF;
+export default generatePDFFromHtml;
